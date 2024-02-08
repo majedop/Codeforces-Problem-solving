404b4b3e3a830cdc82c6297b8ba6bc38b9130edc
majedop <128166712+majedop@users.noreply.github.com>
2024-02-08 23:33:07 +0600

    Create Hexadecimal's_theorem_199A.cpp

---
+++ Hexadecimal's_theorem_199A.cpp
@@ -0,0 +1,12 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+  int n;
+  cin >> n;
+  int a = 0, b = 0; // 0 number is fibonacci
+  cout << a << " " << b << " " << n << endl;
+  
+  return 0;
+}
